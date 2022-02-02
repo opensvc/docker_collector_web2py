f2ecc0e948ea3ef83f99501a3f2e2700f29e9ca2
Arnaud Veron <arnaud.veron@opensvc.com>
2022-02-02 09:28:02 +0100

    Add entrypoint verbosity

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,9 @@ DIR=/docker-entrypoint.d
 
 if [[ -d "$DIR" ]]
 then
+  echo "Begin of docker-entrypoint.d scripts execution"
   /bin/run-parts --verbose "$DIR"
+  echo "End of docker-entrypoint.d scripts execution"
 fi
 
 exec "$@"
